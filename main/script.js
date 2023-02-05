cc35aebbbd7ccf2ef5f5ed27dfb3ddc8a2bc5c0f
free-gen <helios.kms@gmail.com>
2023-02-05 20:06:20 +1000

    -

--- main/script.js
+++ main/script.js
@@ -1,16 +1,68 @@
+let close = "┌─[ ЗАКРЫТЬ (X) ]";
+// Обьекты
+let about = document.querySelector('.about');
+let works = document.querySelector('.works');
+let contacts = document.querySelector('.contacts');
+// Кнопки
+let aboutButton = document.querySelector('.aboutButton');
+let worksButton = document.querySelector('.worksButton');
+let contactsButton = document.querySelector('.contactsButton');
+// Переменные исходного текста из HTML
+let aboutContent = document.querySelector('.aboutButton');
+let aboutText = aboutContent.textContent;
+let worksContent = document.querySelector('.worksButton');
+let worksText = worksContent.textContent;
+let contactsContent = document.querySelector('.contactsButton');
+let contactsText = contactsContent.textContent;
+// ABOUT
+$(aboutButton).click(function() {
+  $(this).toggleClass("active");
+  $('hide').toggleClass("aboutHide");
+  $(works).toggleClass("worksHide");
+  $(contacts).toggleClass("contactsHide");
+  if ( $(this).hasClass( "active" ) ) {
+    $(this).text( close );
+  } else {
+    $(this).text( aboutText );
+  }
+});
+// WORKS
+$(worksButton).click(function() {
+  $(this).toggleClass("active");
+  $('hide').toggleClass("worksHide");
+  $(about).toggleClass("aboutHide");
+  $(contacts).toggleClass("contactsHide");
+  if ( $(this).hasClass( "active" ) ) {
+    $(this).text( close );
+  } else {
+    $(this).text( worksText );
+  }
+});
+// CONTACTS
+$(contactsButton).click(function() {
+  $(this).toggleClass("active");
+  $('hide').toggleClass("contactsHide");
+  $(about).toggleClass("aboutHide");
+  $(works).toggleClass("worksHide");
+  if ( $(this).hasClass( "active" ) ) {
+    $(this).text( close );
+  } else {
+    $(this).text( contactsText );
+  }
+});
+// AUTOTYPING TEXT
 document.addEventListener('DOMContentLoaded',function(event){
   window.onload = function() {
   var dataText = [ 
 		"Современные методы верстки ", 
 		"Стильные визуальные решения ", 
 		"Индивидуальный подход ", 
-		"Решения на любой вкус и кошелек ", 
-		"Стараюсь не оставлять вас равнодушным "
+		"Решения на любой вкус и кошелек "
 	];
   var caret = "|";
   function type(text, i, fnCallback) {
     if (i < (text.length)) {
-      document.querySelector("#text").textContent = text.substring(0, i+1) + caret;
+      document.querySelector(".console").textContent = text.substring(0, i+1) + caret;
       setTimeout(function() {
         type(text, i + 1, fnCallback)
       }, 40);
@@ -23,7 +75,7 @@ document.addEventListener('DOMContentLoaded',function(event){
      if (typeof dataText[i] == 'undefined'){
         setTimeout(function() {
           StartAnimation(0);
-        }, 32000);
+        }, 30000);
      }
     if (i < dataText[i].length) {
       type(dataText[i], 0, function(){
