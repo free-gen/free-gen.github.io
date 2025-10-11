ea639ed31ef5e916cde352095c142c86e30b855f
free-gen <32083948+free-gen@users.noreply.github.com>
2025-10-11 17:43:53 +1000

    Add files via upload

--- filmoteka/script.js
+++ filmoteka/script.js
@@ -135,6 +135,9 @@ function createFilmItem(film, options = {}) {
                         Похожие
                     </button>
                     <button class="btn btn--primary" onclick="watchFilm(${filmId})">
+                        <svg width="12" height="12" viewBox="0 0 448 512" fill="currentColor" style="margin-right: 5px;">
+                            <path d="M91.2 36.9c-12.4-6.8-27.4-6.5-39.6 .7S32 57.9 32 72l0 368c0 14.1 7.5 27.2 19.6 34.4s27.2 7.5 39.6 .7l336-184c12.8-7 20.8-20.5 20.8-35.1s-8-28.1-20.8-35.1l-336-184z"/>
+                        </svg>
                         Смотреть
                     </button>
                 </div>
