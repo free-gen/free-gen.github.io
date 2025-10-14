7e0050fdafa80e2d880005317b75968c4c6937fa
free-gen <32083948+free-gen@users.noreply.github.com>
2025-10-14 21:55:38 +1000

    Delete 101new directory

--- 101new/admin/index.php
+++
@@ -1,1 +0,0 @@
-<? function start($name){if(!file_exists('../'.$name)){$file='start/'.$name;$new_file='../'.$name;copy($file,$new_file);}}start('admin.php');start('pagedot-config.php');header('Location: ../admin.php'); ?>
