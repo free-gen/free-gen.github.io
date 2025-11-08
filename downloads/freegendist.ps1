2e9517630568a534ace81155a31fc93d99ee2308
free-gen <32083948+free-gen@users.noreply.github.com>
2025-11-08 16:08:05 +1000

    Update freegendist.ps1

--- downloads/freegendist.ps1
+++ downloads/freegendist.ps1
@@ -10,6 +10,15 @@ Write-Host "• SetLuma / Управление яркостью монитора
 Write-Host "• Package Installer / Менеджер пакетов Chocolatey" -ForegroundColor White
 Write-Host "• NanoStat / Клиент для NanoStat Device" -ForegroundColor White
 Write-Host ""
+Write-Host "Дополнительно при помощи менеджера winget:" -ForegroundColor White
+Write-Host ""
+Write-Host "• Яндекс Браузер" -ForegroundColor White
+Write-Host "• qBittorrent клиент" -ForegroundColor White
+Write-Host "• Paint.NET" -ForegroundColor White
+Write-Host "• 7zip" -ForegroundColor White
+Write-Host "• Microsoft VSCode" -ForegroundColor White
+Write-Host "• K-Lite Codec Pack" -ForegroundColor White
+Write-Host ""
 Write-Host "Для продолжения установки нажмите любую клавишу..." -ForegroundColor Green
 Write-Host "Для отмены установки закройте окно (Ctrl+C)" -ForegroundColor Red
 
@@ -156,8 +165,28 @@ try {
     Write-Host "⚠ Не удалось добавить в исключения (требуются права администратора)" -ForegroundColor Red
 }
 
+# Установка winget (если не установлен)
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+Write-Host "Winget установлен" -ForegroundColor Gray
+
+# Список пакетов для установки
+$app = @(
+"Yandex.Browser"
+"qBittorrent.qBittorrent"
+"dotPDNLLC.paintdotnet"
+"7zip.7zip"
+"Microsoft.VisualStudioCode"
+"Microsoft.DotNet.SDK.7"
+"Python.Python.3.11"
+"CodecGuide.K-LiteCodecPack.Mega"
+)
+
+# Цикл установки
+foreach ($pkg in $app) {
+    winget install --id $pkg -e --silent --disable-interactivity --accept-package-agreements --accept-source-agreements
+}
+
 Write-Host "`nУстановка завершена!" -ForegroundColor White
 # Write-Host "Все программы установлены в: $InstallPath" -ForegroundColor Gray
 Write-Host "`nДля продолжения нажмите клавишу ВВОД..." -ForegroundColor White
 Read-Host
-
