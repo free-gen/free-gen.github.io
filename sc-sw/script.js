f6b83a6e81ae4bd940ceba6d8031b5917f5df8e1
free-gen <32083948+free-gen@users.noreply.github.com>
2026-07-23 11:31:23 +1000

    Add files via upload

--- sc-sw/script.js
+++ sc-sw/script.js
@@ -1,9 +1,9 @@
 // ServiceWorker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(() => console.log('SW registered'))
-    .catch(err => console.warn('SW failed:', err));
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/sw.js')
+//     .then(() => console.log('SW registered'))
+//     .catch(err => console.warn('SW failed:', err));
+// }
 
 // Набор SVG Иконок
 const ICONS = {
